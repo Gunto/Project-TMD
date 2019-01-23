8e900a26fdbfc91f69cddef5374348a8b969a971
Gunto <trent.braden.williams@gmail.com>
2019-01-23 12:55:32 +1100

    Initial commit: Basic movement and sprite animation

---
+++ TMD/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Animator animator;
+    private Rigidbody2D rb;
+    private Vector3 m_Velocity = Vector3.zero;
+    private float horizontalMove;
+    private float verticalMove;
+
+    [Range(0, .3f)] public float movementSmooth = 0.05f;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+        verticalMove = Input.GetAxisRaw("Vertical") * speed;
+        animator.SetFloat("Horizontal", horizontalMove);
+        animator.SetFloat("Vertical", verticalMove);
+    }
+
+    void FixedUpdate()
+    {
+        Move(horizontalMove * Time.fixedDeltaTime, verticalMove * Time.fixedDeltaTime);
+    }
+
+    private void Move(float horizontal, float vertical)
+    {
+        Vector3 targetVelocity = new Vector2(horizontal * 10f, vertical * 10f);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity, movementSmooth);
+    }
+}
