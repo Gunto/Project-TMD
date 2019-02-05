ebba644c0ac05df1934fd0c341d73c1f958ed061
Gunto <trent.braden.williams@gmail.com>
2019-02-06 08:42:56 +1100

    Camera script

---
+++ TMD/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float smoothing;
+    private Vector3 velocity = Vector3.zero;
+
+    // private void LateUpdate()
+    // {
+    //     Vector3 targetPosition = target.position - offset;
+    //     transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothing * Time.deltaTime);
+    // }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPosition = target.position - offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothing * Time.fixedDeltaTime);
+    }
+}
